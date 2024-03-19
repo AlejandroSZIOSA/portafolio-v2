d09568104496bc4aefd9356be3421899a8df6aea
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-03-19 12:08:34 +0100

    card view

--- src/app/components/main/CardViewProject.jsx
+++ src/app/components/main/CardViewProject.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import Image from "next/image";
 
-/* TODO: FIX IMAGEN SIZE */
 export default function CardViewProject({ project }) {
   const { title, description, imgUrl, gitHubLink } = project;
 
   return (
-    <div className="flex flex-col w-[340] h-[300] border-4 border-indigo-500 rounded-lg bg-[yellow] text-left">
-      <h2 className="text-center">{title}</h2>
+    <div className="flex flex-col w-[340px] h-[300px] border-4 border-indigo-500 rounded-lg bg-[yellow] text-left">
       <Image src={imgUrl} width={200} height={200} alt="no image" />
-      <h3>{description}</h3>
-      <h3>{gitHubLink}</h3>
+
+      <div>
+        <h2 className="text-center">{title}</h2>
+        <h3>{description}</h3>
+      </div>
+      <div>
+        <h3 className="text-center">{gitHubLink}</h3>
+      </div>
     </div>
   );
 }
