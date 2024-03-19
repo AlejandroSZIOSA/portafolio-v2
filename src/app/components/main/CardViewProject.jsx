9aa46a73f3c44eba78a972db35bd480ed2d5f760
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-03-19 14:13:52 +0100

    some changes

--- src/app/components/main/CardViewProject.jsx
+++ src/app/components/main/CardViewProject.jsx
@@ -7,13 +7,13 @@ export default function CardViewProject({ project }) {
   return (
     <div className="flex flex-col w-[340px] h-[300px] border-4 border-indigo-500 rounded-lg bg-[yellow] text-left">
       <Image src={imgUrl} width={200} height={200} alt="no image" />
-
       <div>
         <h2 className="text-center">{title}</h2>
         <h3>{description}</h3>
       </div>
       <div>
         <h3 className="text-center">{gitHubLink}</h3>
+        <a href="https://next-todolist-redux.vercel.app/">To App</a>
       </div>
     </div>
   );
--- src/app/utils/data/projects.js
+++ src/app/utils/data/projects.js
@@ -1,32 +1,43 @@
 import testImg from "/public/projects-img/testImg.png";
 
+const WEB1 = "https://next-todolist-redux.vercel.app/";
+const WEB2 = "https://radio-player-app-chi.vercel.app/";
+const WEB3 = "https://next-todolist-redux.vercel.app/";
+const WEB4 = "https://next-todolist-redux.vercel.app/";
+const WEB5 = "https://next-todolist-redux.vercel.app/";
+const WEB6 = "https://next-todolist-redux.vercel.app/";
+
 export const PROJECTS = [
   {
     id: 1,
-    title: "t1",
-    description: "d1",
     imgUrl: testImg,
+    title: "Todo-App-Redux",
+    description: "d1",
+    linkToApp: WEB1,
     gitHubLink: "g1",
   },
   {
     id: 2,
-    title: "t2",
-    description: "d2",
     imgUrl: testImg,
+    title: "Radio Player",
+    description: "d2",
+    linkToApp: WEB2,
     gitHubLink: "g2",
   },
   {
     id: 3,
+    imgUrl: testImg,
     title: "t3",
     description: "d3",
-    imgUrl: testImg,
+    linkToApp: WEB1,
     gitHubLink: "g3",
   },
   {
     id: 4,
+    imgUrl: testImg,
     title: "t4",
     description: "d4",
-    imgUrl: testImg,
+    linkToApp: WEB1,
     gitHubLink: "g4",
   },
 ];
