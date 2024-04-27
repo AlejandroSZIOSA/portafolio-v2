302c672b5debbd160d7d4ae25d0c37da83e10fbf
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-04-27 18:35:22 +0200

    Hero section changes

--- src/app/components/main/HeroSection.jsx
+++ src/app/components/main/HeroSection.jsx
@@ -1,8 +1,8 @@
 export default function HeroSection() {
   return (
-    <div className="flex flex-col pt-8 items-center justify-center">
-      <div className="w-[340px] h-[340px] bg-white"></div>
-      <div className="ml-4 py-6 text-center">
+    <div className="flex flex-col md:flex-row-reverse pt-8 md:pb-8 items-center justify-center">
+      <div className="w-[340px] h-[340px] md:w-[260px] md:h-[260px] bg-white"></div>
+      <div className="md:w-96 md:h-48 ml-4 md:mr-16 py-6 text-center md:text-left">
         <h2>
           Hi!😎 My name is <strong>Gabriel Sazo, </strong> <br />I am an
           enthusiastic <strong>Frontend Developer</strong> student.
