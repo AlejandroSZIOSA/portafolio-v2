f56ae5e7a06702b064e433369d9bc6788d1cd069
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-04-27 18:40:37 +0200

    Divider Lines changes

--- src/app/components/separators/DividerHeaderTitle.jsx
+++ src/app/components/separators/DividerHeaderTitle.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function DividerHeaderTitle({ sectionTitle }) {
   return (
     <>
-      <h2 className="w-full py-2 text-center font-semibold border-black border-t-4 border-b-4 border-dashed">
+      <h2 className="w-full py-2 text-center font-semibold border-black border-t-2 border-b-2 border-dashed">
         {sectionTitle}
       </h2>
     </>
