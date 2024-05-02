9b60d85834a993061d0863253a283f36bb03a2d6
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-05-02 07:40:05 +0200

    Some changes

--- src/components/main/TechSection.jsx
+++ src/components/main/TechSection.jsx
@@ -4,7 +4,7 @@ import { TECHS } from "/src/utils/data/techs";
 
 export default function TechSection() {
   return (
-    <div className="grid grid-cols-5 md:grid-cols-7 w-80 md:w-full gap-5 md:gap-0 py-5 md:py-9">
+    <div className="grid grid-cols-5 md:grid-cols-6 w-80 md:w-full gap-5 md:gap-0 py-5 md:py-9">
       {TECHS.map((t) => {
         return <TechItem key={t.id} tech={t} />;
       })}
