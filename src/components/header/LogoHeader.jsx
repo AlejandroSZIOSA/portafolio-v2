a64a0b0e3052131f611dae86c30d33aaa0ac3631
Gabriel Sazo <ggato33@gmail.com>
2025-09-27 18:02:24 +0200

    rename header logo and tab title

--- src/components/header/LogoHeader.jsx
+++ src/components/header/LogoHeader.jsx
@@ -8,7 +8,7 @@ export default function LogoHeader() {
   return (
     <div className="flex justify-center items-center ">
       <h1 className="text-[#F24E1E] text-center mt-3 md:ml-4 md:text-4xl">
-        Portafolio
+        Portfolio
       </h1>
       <span className="ml-2">
         <img
