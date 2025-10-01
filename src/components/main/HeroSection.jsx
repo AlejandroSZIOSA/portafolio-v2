a0384542d8fb9ee0b2935694b3d563f2980ff2dc
Gabriel Sazo <ggato33@gmail.com>
2025-10-01 19:21:42 +0200

    Fix hero Section, Add my picture

--- src/components/main/HeroSection.jsx
+++ src/components/main/HeroSection.jsx
@@ -1,11 +1,20 @@
+import Image from "next/image";
+
 export default function HeroSection() {
   return (
     <div className="flex flex-col md:flex-row pt-8 md:pb-8 items-center justify-center">
-      <div className="w-[350px] h-[350px] md:w-[400px] md:h-[400px] bg-white"></div>
+      {/* <div className="w-[350px] h-[350px] md:w-[400px] md:h-[400px] bg-white"></div> */}
+      <Image
+        src={"/images/me/me-black_b.png"}
+        width={300}
+        height={150}
+        alt="no picture"
+        priority
+      />
       <div className="py-6 text-center md:w-96 md:h-auto md:mr-16 md:text-left">
         <p className="font-mono ml-5 m-2 text-left text-lg md:text-2xl">
           Hi!😎 <br></br>My name is <strong>Gabriel Alejandro Sazo. </strong>I
-          am a positive, enthusiastic and curious recently graduated
+          am a positive, curious and high focused recently graduated
           <strong> Frontend Developer. </strong>
         </p>
       </div>
