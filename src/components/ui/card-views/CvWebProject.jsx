3b9c8119c78cb120ad70e8f0b6cfcb236c3b9c9d
AlejandroSZIOSA <ggato33@gmail.com>
2025-12-06 13:11:08 +0100

    Create new reusable component

--- src/components/ui/card-views/CvWebProject.jsx
+++ src/components/ui/card-views/CvWebProject.jsx
@@ -3,15 +3,10 @@ import Image from "next/image";
 import GitHubLinkBtn from "../GitHubLinkBtn";
 import { CONSTANTS } from "@/utils/constants";
 import { useMedia } from "use-media";
-
-const TECH_BOXES_STYLES =
-  "inline-block min-w-fit bg-gray-200 rounded-full content-center px-3 py-1 text-xs font-semibold text-gray-700 text-center";
+import ProjectInfoArea from "./ProjectInfoArea";
 
 export default function CvWebProject({ project }) {
   const isMobile = useMedia(CONSTANTS.USE_MEDIA_MAX_WIDTH); //from useMedia library
-
-  const [mobileUI, desktopUI] = project.responsiveUI; //destructuring assignment
-  const [chrome] = project.browsers;
 
   return (
     // TODO:Fix border Children element in mobile view
@@ -47,59 +42,7 @@ export default function CvWebProject({ project }) {
       <div className="px-2 pb-2 md:px-4 md:pb-4">
         <p className="text-gray-700">{project.description}</p>
       </div>
-      <div className="bg-red-200 border px-1 py-3 flex space-x-2 overflow-x-auto scrollbar-hide md:py-2 md:px-2">
-        {chrome && <span className={TECH_BOXES_STYLES}>{chrome}</span>}
-        {project.deviceBrowserSettings && (
-          <span className={TECH_BOXES_STYLES}>
-            Dev:{project.deviceBrowserSettings}
-          </span>
-        )}
-
-        {project.framework && (
-          <span className={TECH_BOXES_STYLES}>{project.framework}</span>
-        )}
-        {project.language && (
-          <span className={TECH_BOXES_STYLES}>{project.language}</span>
-        )}
-        {project.typeScript && (
-          <span className={TECH_BOXES_STYLES}>TypeScript</span>
-        )}
-
-        {project.navigation && (
-          <span className={TECH_BOXES_STYLES}>{project.navigation}</span>
-        )}
-        {project.context && <span className={TECH_BOXES_STYLES}>Context</span>}
-        {project.redux && <span className={TECH_BOXES_STYLES}>Redux</span>}
-
-        {project.crud && (
-          <span className={TECH_BOXES_STYLES}>C.R.U.D: {project.crud}</span>
-        )}
-
-        {project.localStorage && (
-          <span className={TECH_BOXES_STYLES}>Local Storage</span>
-        )}
-
-        {project.api && (
-          <span className={TECH_BOXES_STYLES}>API:{project.api}</span>
-        )}
-        {project.tests && (
-          <span className={TECH_BOXES_STYLES}>{project.tests}</span>
-        )}
-
-        {project.css && (
-          <span className={TECH_BOXES_STYLES}>{project.css}</span>
-        )}
-        {project.cssLibrary && (
-          <span className={TECH_BOXES_STYLES}>{project.cssLibrary}</span>
-        )}
-
-        {mobileUI && <span className={TECH_BOXES_STYLES}>Mobile UI</span>}
-        {desktopUI && <span className={TECH_BOXES_STYLES}>Desktop UI</span>}
-
-        {project.wcag && (
-          <span className={TECH_BOXES_STYLES}>Wcag:{project.wcag}</span>
-        )}
-      </div>
+      <ProjectInfoArea project={project} />
       <div className="px-6 py-3 flex justify-between items-center md:py-4">
         <a
           href={project.linkToApp}
---
+++ src/components/ui/card-views/ProjectInfoArea.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const TECH_BOXES_STYLES =
+  "inline-block min-w-fit bg-gray-200 rounded-full content-center px-3 py-1 text-xs font-semibold text-gray-700 text-center";
+
+export default function ProjectInfoArea({ project }) {
+  const [mobileUI, desktopUI] = project.responsiveUI; //destructuring assignment
+  const [chrome] = project.browsers;
+  return (
+    <div className="bg-red-200 border px-1 py-3 flex space-x-2 overflow-x-auto scrollbar-hide md:py-2 md:px-2">
+      {chrome && <span className={TECH_BOXES_STYLES}>{chrome}</span>}
+      {project.deviceBrowserSettings && (
+        <span className={TECH_BOXES_STYLES}>
+          Dev:{project.deviceBrowserSettings}
+        </span>
+      )}
+      {project.framework && (
+        <span className={TECH_BOXES_STYLES}>{project.framework}</span>
+      )}
+      {project.language && (
+        <span className={TECH_BOXES_STYLES}>{project.language}</span>
+      )}
+      {project.typeScript && (
+        <span className={TECH_BOXES_STYLES}>TypeScript</span>
+      )}
+
+      {project.navigation && (
+        <span className={TECH_BOXES_STYLES}>{project.navigation}</span>
+      )}
+      {project.context && <span className={TECH_BOXES_STYLES}>Context</span>}
+      {project.redux && <span className={TECH_BOXES_STYLES}>Redux</span>}
+
+      {project.crud && (
+        <span className={TECH_BOXES_STYLES}>C.R.U.D: {project.crud}</span>
+      )}
+
+      {project.localStorage && (
+        <span className={TECH_BOXES_STYLES}>Local Storage</span>
+      )}
+
+      {project.api && (
+        <span className={TECH_BOXES_STYLES}>API:{project.api}</span>
+      )}
+      {project.tests && (
+        <span className={TECH_BOXES_STYLES}>{project.tests}</span>
+      )}
+
+      {project.css && <span className={TECH_BOXES_STYLES}>{project.css}</span>}
+      {project.cssLibrary && (
+        <span className={TECH_BOXES_STYLES}>{project.cssLibrary}</span>
+      )}
+
+      {mobileUI && <span className={TECH_BOXES_STYLES}>Mobile UI</span>}
+      {desktopUI && <span className={TECH_BOXES_STYLES}>Desktop UI</span>}
+
+      {project.wcag && (
+        <span className={TECH_BOXES_STYLES}>Wcag:{project.wcag}</span>
+      )}
+    </div>
+  );
+}
