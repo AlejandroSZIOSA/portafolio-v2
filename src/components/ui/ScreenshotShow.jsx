14c4d75e3c820afa50800ec52139a2717f443e25
AlejandroSZIOSA <ggato33@gmail.com>
2025-12-19 22:03:45 +0100

    remove unnecessary component

--- src/components/ui/ScreenshotShow.jsx
+++
@@ -1,5 +0,0 @@
-const ScreenshotShow = ({ url }) => {
-  return <img src={url} width={240} height={100} alt="no card image" />;
-};
-
-export default ScreenshotShow;
--- src/components/ui/details-page/ScreenShotScrollView.jsx
+++ src/components/ui/details-page/ScreenShotScrollView.jsx
@@ -1,12 +1,10 @@
-import ScreenshotShow from "../ScreenshotShow";
-
 export default function ScreenShotScrollView({ screenshots }) {
   return (
     <div className="bg-[#deb887] border px-1 py-2 overflow-x-auto h-[10-vh] w-screen lg:w-[70vw]">
       <ol className="flex items-center ml-1">
         {screenshots.map((url, index) => (
           <li key={index} className="flex-shrink-0 mx-2">
-            <ScreenshotShow url={url} />
+            <img src={url} width={240} height={100} alt="no card image" />
           </li>
         ))}
       </ol>
