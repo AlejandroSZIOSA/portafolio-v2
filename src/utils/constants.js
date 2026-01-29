b6ac328205d2ceb5144ade20e295e2e74beeb946
Gabriel Sazo <ggato33@gmail.com>
2026-01-29 16:42:55 +0100

    date fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 //Constants
 export const CONSTANTS = {
   USE_MEDIA_MAX_WIDTH: "(max-width: 765px)", //global mobile breakpoint for useMedia library
-  CURRENT_UPDATED_DATE: "January 15, 2026",
+  CURRENT_UPDATED_DATE: "January 29, 2026",
 };
