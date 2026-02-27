59c2aa28a6ee929acc3cec3e9466f632e1a1171b
Gabriel Sazo <ggato33@gmail.com>
2026-02-27 21:15:50 +0100

    cleaning the code

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -4,23 +4,15 @@ import * as DATA_PROJECTS from "@/utils/data/index";
 export function getDataProjects(dataIdentifier) {
   switch (dataIdentifier) {
     case "mobileUIwebApps":
-      return DATA_PROJECTS.WEB_DATA.filter(
-        (item) => item.responsiveUI[0] && !item.isNew,
-      );
-    case "desktopUIwebApps":
-      return DATA_PROJECTS.WEB_DATA.filter(
-        (item) => item.responsiveUI[1] && !item.isNew,
-      );
+      return DATA_PROJECTS.WEB_DATA.filter((item) => item.responsiveUI[0]);
     case "mobileUIwebApis":
-      return DATA_PROJECTS.WEB_APIS_DATA.filter(
-        (item) => item.responsiveUI[0] && !item.isNew,
-      );
+      return DATA_PROJECTS.WEB_APIS_DATA.filter((item) => item.responsiveUI[0]);
+    case "desktopUIwebApps":
+      return DATA_PROJECTS.WEB_DATA.filter((item) => item.responsiveUI[1]);
     case "desktopUIwebApis":
-      return DATA_PROJECTS.WEB_APIS_DATA.filter(
-        (item) => item.responsiveUI[1] && !item.isNew,
-      );
+      return DATA_PROJECTS.WEB_APIS_DATA.filter((item) => item.responsiveUI[1]);
     case "webBackendDbProjects": // exception for responsive UI
-      return DATA_PROJECTS.WEB_BACKEND_DB_DATA.filter((item) => !item.isNew);
+      return DATA_PROJECTS.WEB_BACKEND_DB_DATA;
     default:
       return [];
   }
