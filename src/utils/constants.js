e57b5a3a88ab0d81213df9a28c2e70bcf0f5eb4a
Gabriel Sazo <ggato33@gmail.com>
2026-02-28 06:34:51 +0100

    refactoring

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const CONSTANTS = {
   USE_MEDIA_MAX_WIDTH: "(max-width: 765px)", //global mobile breakpoint for useMedia library
-  CURRENT_UPDATED_DATE: "February 26, 2026",
+  CURRENT_UPDATED_DATE: "February 28, 2026",
 };
