47ed702a2aed442c1a2ac1c8b953e0bde1e87f36
AlejandroSZIOSA <ggato33@gmail.com>
2026-03-05 00:13:37 +0100

    retouches

--- src/components/ui/ConstructionMessage.jsx
+++ src/components/ui/ConstructionMessage.jsx
@@ -1,7 +1,20 @@
+import { useMedia } from "use-media";
+import Image from "next/image";
+import { CONSTANTS } from "../../utils/constants";
+import meditationIcon from "../../../public/icons/self-improvement.svg";
+
 export default function ConstructionMessage() {
+  const isMobile = useMedia(CONSTANTS.USE_MEDIA_MAX_WIDTH); //from useMedia library
   return (
     <div className="flex h-10 lg:h-[549px] items-center justify-center">
-      <h2 className="text-center">In Progress... </h2>
+      <p className="text-center"> - _ _ </p>
+      <Image
+        src={meditationIcon}
+        width={isMobile ? "38" : "90"}
+        height="auto"
+        alt="In Progress"
+      />
+      <span> _ _ -</span>
     </div>
   );
 }
