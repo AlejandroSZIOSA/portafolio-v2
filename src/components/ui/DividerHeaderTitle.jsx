06b8c1b8144bdff31998555edced66216d185131
Gabriel Sazo <ggato33@gmail.com>
2026-03-06 18:51:55 +0100

    fix line divider on mobile ui

--- src/components/ui/DividerHeaderTitle.jsx
+++ src/components/ui/DividerHeaderTitle.jsx
@@ -1,3 +1,6 @@
+const TOP_LINE = "border-b-0 lg:border-b-2 lg:border-t-2"; //lg:border-b-2 applies only desktop
+const BOTH_LINES = "border-b-2";
+
 export default function DividerHeaderTitle({
   sectionTitle,
   putSectionId,
@@ -8,8 +11,8 @@ export default function DividerHeaderTitle({
       /* I am using the Id attribute to link the section to the anchor tag */
       id={putSectionId}
       className={`
-        w-full py-2 text-center font-semibold border-black border-dashed border-b-2 border-t-2 lg:border-b-2 ${
-          variant === "top-line" && "border-b-0"
+        w-full py-2 text-center font-semibold border-black border-dashed border-t-2 ${
+          variant === "top-line" ? TOP_LINE : BOTH_LINES
         }
       `}
     >
