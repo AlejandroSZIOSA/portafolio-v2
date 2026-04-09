190df7f2f27db7943f68df20346295bbd09f5333
Gabriel Sazo <ggato33@gmail.com>
2026-04-09 19:32:26 +0200

    fix project preview image

--- src/utils/data/web-privateApis.js
+++ src/utils/data/web-privateApis.js
@@ -2,7 +2,7 @@ export const WEB_PRIVATE_APIS = [
   {
     id: 7,
     updated_at: "April 04, 2026",
-    cardImgUrl: "/images/web-private-apis-p/weather/we-app.png",
+    cardImgUrl: "/images/web-private-apis-p/weather/we2-app.png",
     title: "Weather",
     isFavorite: true,
     version: "Stable",
