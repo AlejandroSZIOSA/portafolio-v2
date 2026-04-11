668badb4fe3fd002703606ff1d7920e53c8820c9
Gabriel Sazo <ggato33@gmail.com>
2026-04-11 21:51:50 +0200

    app gallery navigation , fix

--- src/components/ui/tabs/TabsList.jsx
+++ src/components/ui/tabs/TabsList.jsx
@@ -1,7 +1,7 @@
 // TabsList component
 export function TabsList({ children }) {
   return (
-    <div className="grid grid-cols-2 gap-1 pb-4 bg-[#FFE4C4] justify-around border-b border-black lg:flex lg:flex-flex lg:w-max lg:pb-0 lg:justify-center lg:my-1 lg:mx-auto lg:border-4 lg:rounded-lg">
+    <div className="grid grid-cols-2 gap-1 pb-4 bg-[#FFE4C4] justify-around border-b border-black lg:flex lg:flex-flex lg:w-max lg:pb-0 lg:justify-center lg:my-1 lg:mx-auto lg:border-2">
       {children}
     </div>
   );
--- src/components/ui/tabs/TabsTrigger.jsx
+++ src/components/ui/tabs/TabsTrigger.jsx
@@ -1,9 +1,9 @@
 import { useTabs } from "@/hooks/useTabs";
 
 //dynamic classes for active and inactive tabs
-const BASE_TAB = "p-1 hover:cursor-pointer lg:px-5 lg:py-2";
+const BASE_TAB = "p-1 hover:cursor-pointer lg:px-2 lg:py-2";
 const ACTIVE_TAB =
-  "border-4 lg:border-0 border-black lg:text-[navy] font-semibold";
+  "border-4 lg:border-0 border-black lg:text-[floralwhite] lg:bg-[cadetblue] font-semibold";
 const INACTIVE_TAB = "text-gray-500";
 
 // TabsTrigger component
@@ -16,7 +16,7 @@ export function TabsTrigger({ value, children, lengthProjects = [] }) {
       onClick={() => setActiveTab(value)}
       className={`${BASE_TAB} ${isActive ? ACTIVE_TAB : INACTIVE_TAB}`}
     >
-      <h3 /* className="lg:text-xl" */>
+      <h3 className="lg:text-base">
         {children}
         <span>
           <strong>{`(+${lengthProjects})`}</strong>
