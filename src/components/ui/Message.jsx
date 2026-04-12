f9b817a78c2ad90ff390f933c993e4a7a844755c
Gabriel Sazo <ggato33@gmail.com>
2026-04-12 21:50:25 +0200

    fixing my messages boxes

--- src/components/ui/Message.jsx
+++ src/components/ui/Message.jsx
@@ -4,13 +4,14 @@ export default function Message({ children, variant }) {
   let content;
   switch (variant) {
     case "my-current-focus":
-      content = "mb-3 bg-[#00CED1] border-black text-black lg:w-fit";
+      content =
+        "mb-3 bg-[#00CED1] border-black text-black lg:w-fit lg:absolute lg:bottom-0 ";
       break;
     case "my-hobbies":
       content = " bg-[#70A069] border-black text-white lg:w-fit";
       break;
     case "my-wish":
-      content = " bg-black border-[#FF5A01] text-green-400 lg:w-fit";
+      content = " bg-black border-[#FF5A01] opacity-75 text-green-400 lg:w-fit";
       break;
     default:
       content = "bg-gray-red-500 text-white";
@@ -19,9 +20,8 @@ export default function Message({ children, variant }) {
 
   return (
     <div
-      className={`inline-flex w-11/12 border-2 mt-2 ${content} text-center font-mono p-1 lg:hidden `}
+      className={`inline-flex w-11/12 border-2 mt-2 ${content} text-center font-mono p-1 ${variant === "my-current-focus" && "lg:absolute lg:bottom-0 lg:left-0"}`}
     >
-      {/*   <span className="absolute left-3">🎯</span> */}
       <p className="py-1">{children}</p>
     </div>
   );
