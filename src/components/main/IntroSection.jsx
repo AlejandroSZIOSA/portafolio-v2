5ad9429ec46d968c8cc7179c14c7ed636162e49c
AlejandroSZIOSA <ggato33@gmail.com>
2026-04-21 15:21:11 +0200

    fix messages intro section

--- src/components/main/IntroSection.jsx
+++ src/components/main/IntroSection.jsx
@@ -10,29 +10,41 @@ import Message from "../ui/Message";
 export default function IntroSection() {
   const isMobile = useMedia(CONSTANTS.USE_MEDIA_MAX_WIDTH); //from useMedia library
   return (
-    <div className="flex flex-col pt-4 items-center justify-center md:flex-row md:pt-8 md:pb-12 md:mr-auto md:pl-16">
+    <div className="flex flex-col pt-4 w-full items-center justify-center md:flex-row md:pb-6 md:mr-auto md:pl-16">
       <div>
         <Image
           src={me}
-          className="rounded-[15%]"
-          width={isMobile ? 310 : 400}
+          className="rounded-[15%] w-[310px] lg:w-[550px]"
+          /* width={isMobile ? 310 : 400} */
           height="auto"
           alt="no picture"
           loading="eager" //is like priority in next/image, it forces the image to load as soon as possible, which is good for above-the-fold images like profile pictures.
         />
         <LanguagePanel />
       </div>
-      <div className="py-2 my-3 text-center w-11/12 border-2 border-black rounded-xl bg-[#9CB3A5] lg:text-left lg:w-96 lg:h-auto lg:py-0 lg:mt-auto lg:ml-11">
+      {/*  <div className="py-2 my-3 text-center w-11/12 border-2 border-black rounded-xl bg-[#9CB3A5] lg:text-left lg:w-96 lg:h-fit lg:py-0 lg:mt-auto lg:ml-11">
         <p className="font-mono ml-3 p-2 text-left text-lg lg:ml-4 lg:p-3 lg:text-xl">
           Hi!😎 <br></br>My name is{" "}
           <strong id="intro_text_strong">Gabriel Alejandro</strong>
           <br></br>I am an action oriented, highly focused and very curious
           <strong> Frontend Developer Junior.</strong>
         </p>
-      </div>
+      </div> */}
       {/* TODO: fix to desktop ui
        */}
-      {/*   <div className="flex flex-col items-center lg:items-stretch lg:flex-row-reverse lg:relative lg:h-[stretch] lg:bg-slate-300">
+      <div
+        id="messages-container"
+        className="flex flex-col items-center lg:flex-row-reverse lg:w-[stretch] lg:items-stretch lg:relative lg:h-[stretch] lg:mx-5"
+      >
+        <div className="py-2 my-3 text-center w-11/12 border-2 lg:border-4 border-black rounded-xl bg-[#9CB3A5] lg:text-left lg:w-[350px] lg:h-[250px] lg:py-0 lg:mt-auto lg:mb-0 lg:absolute lg:left-0 bottom-[8%]">
+          <p className="font-mono p-2 text-left text-lg lg:p-4 lg:text-xl">
+            Hi!😎 <br></br>My name is{" "}
+            <strong id="intro_text_strong">Gabriel Alejandro</strong>
+            <br></br>I am an action oriented, highly focused and very curious
+            <strong> Frontend Developer Junior.</strong>
+          </p>
+        </div>
+
         <Message variant="my-wish">
           const MY_WISH:”Become a multi-platform fullstack Javascript developer”
         </Message>
@@ -44,7 +56,7 @@ export default function IntroSection() {
           <span className="mr-2">🎯</span>
           {"let currentFocus:{backend:[“Architecture”,“Good practices”]}"}
         </Message>
-      </div> */}
+      </div>
     </div>
   );
 }
